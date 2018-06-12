c3b63b89b869f277ebe937ce14c5f0f788a61ffe
Patrick Leung <pleung1987@gmail.com>
2018-06-12 14:33:51 +0800

    ui updated

--- Angular-app/src/app/app.component.ts
+++ Angular-app/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { HttpService } from './http.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent implements OnInit {
