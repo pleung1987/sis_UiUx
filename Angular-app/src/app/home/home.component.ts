cc105fbdc2aff9abd2f23af1db04164e75776248
Patrick Leung <pleung1987@gmail.com>
2018-06-22 16:28:24 +0800

    solved filter feature

--- Angular-app/src/app/home/home.component.ts
+++ Angular-app/src/app/home/home.component.ts
@@ -11,9 +11,26 @@ import 'rxjs/add/observable/throw';
 })
 export class HomeComponent implements OnInit, OnDestroy {
   visits: any;
+  filteredVisits: any;
+  shops: any;
   Refresh = Observable.interval(2000); // set refresh rate
   subscription: any;
   stopped: Boolean = true;
+
+  private _shopChosen: string;
+  get shopChosen(): string {
+    return this._shopChosen;
+  }
+  set shopChosen(value: string) {
+    this._shopChosen = value;
+    this.filteredVisits = this.filterVisits(value);
+  }
+
+  filterVisits(searchString: string): void {
+    console.log('this is the searchString: ', searchString);
+    return this.visits.filter( visit =>
+      visit._camera._shop.branch.toLowerCase().indexOf(searchString.toLowerCase()) !== -1);
+  }
 
   constructor(
     private _route: ActivatedRoute,
@@ -23,17 +40,31 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.getVisits();
+    this.getShops();
     this.resume();
   }
   ngOnDestroy(): void {
     this.stop();
+    console.log('this is the chosen: ', this.shopChosen);
   }
 
   getVisits(): void {
     const visitsObservable = this._httpService.shareVisits();
     visitsObservable.subscribe(data => {
       this.visits = data['data'];
+      this.filteredVisits = this.visits;
+      this.filteredVisits = this.filterVisits(this.shopChosen);
       console.log('this is the visit data: ', this.visits);
+      console.log('this is the filterVisit data: ', this.filteredVisits);
+    });
+  }
+
+  getShops(): void {
+    const shopObservable = this._httpService.shareShops();
+    shopObservable.subscribe(data => {
+      this.shops = data;
+      this.shops.splice(0, 0, '');
+      console.log('this is the shop data: ', this.shops);
     });
   }
 
